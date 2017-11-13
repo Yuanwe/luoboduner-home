d4c6228052186aa5b9067b3e22ae600e6bea2c3b
zhouy <rememberber@163.com>
2017-11-13 16:39:35 +0800

    gallery demo

--- src/components/PhotoPage.vue
+++ src/components/PhotoPage.vue
@@ -1,13 +1,49 @@
 <template>
-  <section class="fdb-block" id="call-to-action">
-    <div class="container">
-      <p>Photo is comming soon</p>
+  <div class="row">
+    <div class="col-lg-12">
+      <div class="lightBoxGallery">
+        <a href="https://farm5.staticflickr.com/4324/35722977410_13ab0aa79d_k.jpg" title="Image" data-gallery=""><img src="https://farm5.staticflickr.com/4324/35722977410_13ab0aa79d_k.jpg"></a>
+        <a href="https://farm5.staticflickr.com/4324/35722977410_13ab0aa79d_k.jpg" title="Image" data-gallery=""><img src="https://farm5.staticflickr.com/4324/35722977410_13ab0aa79d_k.jpg"></a>
+        <a href="https://farm5.staticflickr.com/4324/35722977410_13ab0aa79d_k.jpg" title="Image" data-gallery=""><img src="https://farm5.staticflickr.com/4324/35722977410_13ab0aa79d_k.jpg"></a>
+        <a href="https://farm5.staticflickr.com/4324/35722977410_13ab0aa79d_k.jpg" title="Image" data-gallery=""><img src="https://farm5.staticflickr.com/4324/35722977410_13ab0aa79d_k.jpg"></a>
+        <a href="https://farm5.staticflickr.com/4324/35722977410_13ab0aa79d_k.jpg" title="Image" data-gallery=""><img src="https://farm5.staticflickr.com/4324/35722977410_13ab0aa79d_k.jpg"></a>
+        <a href="https://farm5.staticflickr.com/4324/35722977410_13ab0aa79d_k.jpg" title="Image" data-gallery=""><img src="https://farm5.staticflickr.com/4324/35722977410_13ab0aa79d_k.jpg"></a>
+        <a href="https://farm5.staticflickr.com/4324/35722977410_13ab0aa79d_k.jpg" title="Image" data-gallery=""><img src="https://farm5.staticflickr.com/4324/35722977410_13ab0aa79d_k.jpg"></a>
+        <a href="https://farm5.staticflickr.com/4324/35722977410_13ab0aa79d_k.jpg" title="Image" data-gallery=""><img src="https://farm5.staticflickr.com/4324/35722977410_13ab0aa79d_k.jpg"></a>
+        <a href="https://farm5.staticflickr.com/4324/35722977410_13ab0aa79d_k.jpg" title="Image" data-gallery=""><img src="https://farm5.staticflickr.com/4324/35722977410_13ab0aa79d_k.jpg"></a>
+        <a href="https://farm5.staticflickr.com/4324/35722977410_13ab0aa79d_k.jpg" title="Image" data-gallery=""><img src="https://farm5.staticflickr.com/4324/35722977410_13ab0aa79d_k.jpg"></a>
+        <a href="https://farm5.staticflickr.com/4324/35722977410_13ab0aa79d_k.jpg" title="Image" data-gallery=""><img src="https://farm5.staticflickr.com/4324/35722977410_13ab0aa79d_k.jpg"></a>
+        <a href="https://farm5.staticflickr.com/4324/35722977410_13ab0aa79d_k.jpg" title="Image" data-gallery=""><img src="https://farm5.staticflickr.com/4324/35722977410_13ab0aa79d_k.jpg"></a>
+        <a href="https://farm5.staticflickr.com/4324/35722977410_13ab0aa79d_k.jpg" title="Image" data-gallery=""><img src="https://farm5.staticflickr.com/4324/35722977410_13ab0aa79d_k.jpg"></a>
+
+        <!-- The Gallery as lightbox dialog, should be a child element of the document body -->
+        <div id="blueimp-gallery" class="blueimp-gallery">
+          <div class="slides"></div>
+          <h3 class="title"></h3>
+          <a class="prev">‹</a>
+          <a class="next">›</a>
+          <a class="close">×</a>
+          <a class="play-pause"></a>
+          <ol class="indicator"></ol>
+        </div>
+
+      </div>
     </div>
-  </section>
+
+  </div>
 </template>
 
 <style scoped>
-  #call-to-action {
-    background-image: url("../assets/imgs/bg_c_1.svg")
+  .lightBoxGallery {
+    text-align: center;
+  }
+
+  .lightBoxGallery img {
+    margin: 2px;
+    height: 160px;
+    width: 240px;
   }
 </style>
+
+<script>
+</script>
